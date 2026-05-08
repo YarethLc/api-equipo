38130e9456892a7a9c435f0b494879f79ab7aabe
MARKO ANDRES ARCILA ZAPATA <MARKO.ARCILA01@uceva.edu.co>
2026-05-08 10:19:06 -0500

    feat: agregar endpoint GET /equipo/Marko

--- src/main/java/com/equipo/api/EquipoController.java
+++ src/main/java/com/equipo/api/EquipoController.java
@@ -19,21 +19,18 @@ public class EquipoController {
     // ZONA DE TRABAJO DEL EQUIPO
     // ─────────────────────────────────────────────────────────────────
 
- feature/Stefany
-    @GetMapping("/Maria Stefany Izquierdo Palacios")
-    public String saludo() {
+    @GetMapping("/stefany")
+    public String saludoStefany() {
         return "Hola, soy Stefany y este es mi endpoint.";
-
-    @GetMapping("/Yareth")
-    public String saludo() {
-        return "Hola, soy Yareth y este es mi endpoint.";
-        
-    @GetMapping("/esteban")
-    public String saludo() {
-        return "Hola, soy esteban y este es mi endpoint.";
- develop
     }
 
+    @GetMapping("/yareth")
+    public String saludoYareth() {
+        return "Hola, soy Yareth y este es mi endpoint.";
+    }
+
+    @GetMapping("/esteban")
+    public String saludoEsteban() {
+        return "Hola, soy Esteban y este es mi endpoint.";
+    }
 }
-
-
